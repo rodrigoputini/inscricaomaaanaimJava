765f7febf6c4baf6a65570a67e7c88c9f7a90355
Rodrigo Putini <rodrigo.putini@gmail.com>
2015-02-27 16:52:46 -0300

    adjusts

---
+++ NetBeansProjects/EventosCuritiba/eventoscwb-war/src/java/br/org/icmcuritiba/to/PessoaTO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.icmcuritiba.to;
+
+import java.util.List;
+import br.org.icmcuritiba.entity.Pessoa;
+
+/**
+ *
+ * @author s016361
+ */
+public class PessoaTO {
+    
+    private List<Pessoa> lstPessoa;
+
+    public List<Pessoa> getLstPessoa() {
+        return lstPessoa;
+    }
+
+    public void setLstPessoa(List<Pessoa> lstPessoa) {
+        this.lstPessoa = lstPessoa;
+    }
+    
+    
+    
+}
