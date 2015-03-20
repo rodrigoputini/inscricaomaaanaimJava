50ae8734613f4b6aaea4b5cdafcea0a727bdaea0
Rodrigo Putini <rodrigo.putini@gmail.com>
2015-03-20 17:09:11 -0300

    remocao

--- eventos/app/js/controllers/homeCtrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.controller('homeCtrl', ['$scope','loginService', function($scope,loginService){
-	$scope.txt='/Inicio';
-	loginService.setMenus($scope);
-	$scope.logout=function(){
-		loginService.logout();
-	}
-}])
