31e74abc5bfb1ed34140800e598c39d3f103bece
Rodrigo Putini <rodrigo.putini@gmail.com>
2015-04-13 16:58:19 -0300

    modificacoes uteis

--- database/selectByCep.sql
+++ database/selectByCep.sql
@@ -1,11 +1,17 @@
-﻿select logr.log_tipo_logradouro as tipo, 
-logr.log_no_sem_acento as logradrouro,
+select 0 as codendereco, 
+logr.log_tipo_logradouro ||' '|| 
+logr.log_no_sem_acento as logradouro,
 bairro.bai_no bairro,
-loc.loc_no as localidade,
-loc.ufe_sg as estado
+loc.loc_no as cidade,
+loc.ufe_sg as estado,
+''as pais,
+logr.cep as cep
 from cep.log_logradouro logr,
 cep.log_bairro bairro,
 cep.log_localidade loc
 where bairro.bai_nu_sequencial  =  logr.bai_nu_sequencial_ini 
 and bairro.loc_nu_sequencial = loc.loc_nu_sequencial
-and logr.cep = '82630160'
+and logr.cep = '81630060'
+
+
+
